49556d9d0aab63213d00bc113e1d6b6dc531f093
unknown <kevinmcshane02@gmail.com>
2023-04-21 11:26:38 +0000

    MA_lab9

---
+++ src/app/status/status.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Storage } from "@ionic/storage-angular";
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.page.html',
+  styleUrls: ['./status.page.scss'],
+})
+export class StatusPage implements OnInit {
+
+  myStatus:string = "";
+
+  constructor(private storage:Storage, private navController:NavController) { }
+
+  ngOnInit() {
+  }
+
+  async onSave(){
+    await this.storage.create();
+    await this.storage.set("status", this.myStatus);
+    await this.navController.navigateBack('/home');
+    
+  }
+
+}
+
